fac218cc6af434143c388283c8fee0d64db699c6
tylerheckel2 <tylerheckel2@gmail.com>
2024-06-22 20:34:31 -0400

    Began a work in progress new level design that auto generates the terrain.
    Still working on this

--- BreakingStuffGame/Assets/Scripts/TerrainHandler.cs
+++ BreakingStuffGame/Assets/Scripts/TerrainHandler.cs
@@ -4,21 +4,90 @@ using UnityEngine;
 
 public class TerrainHandler : MonoBehaviour
 {
+    public int dirtLayerHeight = 5;
+    
+    public Sprite dirt;
+    public Sprite grass;
+    public Sprite stone;
+
     private GameObject[] worldChunks;
     private List<Vector2> worldTiles = new List<Vector2>();
     private List<GameObject> worldTileObjects = new List<GameObject>();
+
+    public bool generateCaves = true;
+
     public int worldSize = 100;
     public int chunkSize = 16;
-    // Start is called before the first frame update
-    void Start()
+
+    public float surfaceValue = 0.25f;
+    public float caveFreq = 0.05f;
+    public float terrainFreq = 0.05f;
+    public float heightMultiplier = 4f;
+    public int heightAddition = 25;
+
+    public float seed;
+    public Texture2D noiseTexture;
+
+    private void Start()
     {
-        
+        seed = Random.Range(-10000, 10000);
+        GenerateNoiseTexture();
+        GenerateTerrain();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GenerateTerrain()
     {
-        
+        for (int x = 0; x < worldSize; x++)
+        {
+            float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
+            for (int y = 0; y < height; y++)
+            {
+                Sprite tileSprite;
+                if (y < height - dirtLayerHeight)
+                {
+                    tileSprite = stone;
+                }
+                else if (y < height - 1)
+                {
+                    tileSprite = dirt;
+                }
+                else
+                {
+                    tileSprite = grass;
+                }
+
+                if (generateCaves)
+                {
+                    if (noiseTexture.GetPixel(x, y).r > surfaceValue)
+                    {
+                        PlaceTiler(tileSprite, x, y);
+                    }
+                }
+                else  
+                {
+                    PlaceTiler(tileSprite, x, y);
+                }
+            }
+        }
+    }
+
+
+    public void GenerateNoiseTexture()
+    {
+        noiseTexture = new Texture2D(worldSize, worldSize);
+
+        for (int x=0; x < noiseTexture.width; x++)
+        {
+            for (int y = 0; y < noiseTexture.height; y++)
+            {
+                float v = Mathf.PerlinNoise((x + seed) * caveFreq, (y + seed) * caveFreq);
+                noiseTexture.SetPixel(x, y, new Color(v, v, v));
+
+            }
+        }
+
+        noiseTexture.Apply();
+
     }
 
     public void CreateChunks()
@@ -73,5 +142,15 @@ public class TerrainHandler : MonoBehaviour
             worldTileObjects.Add(newTile);
 
         }
+    }
+
+    public void PlaceTiler(Sprite tileSprite, float x, float y)
+    {
+        GameObject newTile = new GameObject();
+        newTile.transform.parent = this.transform;
+        newTile.AddComponent<SpriteRenderer>();
+        newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+        newTile.name = tileSprite.name;
+        newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
     }
 }
