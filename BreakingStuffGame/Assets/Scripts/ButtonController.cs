b779822cb6c0181ff0c951a59e58144eb5bf7761
tylerheckel2 <tylerheckel2@gmail.com>
2024-07-29 19:02:47 -0400

    Added additional UI and button navigation, implemented edit to magic rock tile

---
+++ BreakingStuffGame/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(sceneName: "StartScreen");
+    }
+}
